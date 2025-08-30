69bd94d779aedf8e19aacc0e52bdceb43ba39bcc
victorokonkwo67@gmail.com <victorokonkwo67@gmail.com>
2025-08-30 02:52:28 +0100

    feat: add another way to solve fib

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 fn main() {
+    // using f(n) = f(n - 1) + f(n - 2)
     let fib_number = 12;
     let result = fibonacci(fib_number); // time complexity = O(n)
 
+    let res = fibonacci_2(fib_number as usize);
+
     println!("{result}");
+    println!("{res}");
 }
 
 fn fibonacci(n: u64) -> u64 {
     match n {
         // handle base cases
-        0 => return 0,
-        1 => return 1,
+        0 => 0,
+        1 => 1,
 
         // match every other case
         _ => {
-            // using f(n) = f(n - 1) + f(n - 2)
             let mut f0 = 0;
             let mut f1 = 1;
             let mut fib = 0;
@@ -24,7 +27,17 @@ fn fibonacci(n: u64) -> u64 {
                 f1 = fib;
             }
 
-            return fib;
+            fib
         }
     }
+}
+
+fn fibonacci_2(n: usize) -> u32 {
+    let mut ans = vec![0, 1];
+
+    for i in 2..=n {
+        ans.push(ans[i - 1] + ans[i - 2]);
+    }
+
+    ans[n]
 }
